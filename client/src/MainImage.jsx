60975c1765658243522cff4ba2a3b1675bf2f227
Shelly <pxshelly@gmail.com>
2019-02-18 16:07:56 -0800

    Implement feature to zoom in on main image

--- client/src/MainImage.jsx
+++ client/src/MainImage.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
+const xMark = {
+  d: 'path("M7 7l10 10m0-10L7 17")'
+}
+
 class MainImage extends React.Component {
   constructor(props) {
     super(props);
     this.state={
       currentImage: props.mainImage,
-      zoom: true;
+      zoom: false
     };
-
     // this.handleClick=this.handleClick.bind(this);
+    this.zoomIn = this.zoomIn.bind(this);
   }
 
+  zoomIn() {
+    console.log('zoom');
+    this.setState({zoom: !this.state.zoom})
+  }
 
   // handleClick(event) {
   //   event.preventDefault();
@@ -20,17 +28,33 @@ class MainImage extends React.Component {
 
 
   render() {
-    return (
-      <div>
-        <div className='mainImageContainer'>
-          {/* <div id='mainImage' > */}
-            <img src={this.state.currentImage} id='mainImage'/>
-          {/* </div> */}
+    if (this.state.zoom) {
+      return (
+        <div className = 'zoomInWrapper'>
+          <img src={this.state.currentImage} className ='zoomMainImage'/>
+          <div className ='exit'>
+            <svg focusable="false" height="24" width="24" class='exitIconPosition'>
+              <g>
+                <circle class='circle' cx="12" cy="12" r="11"></circle>
+              <path class='x' style={xMark}></path>
+              </g>
+            </svg>
+          </div>
         </div>
-        {/* <div className = 'zoom'>
-        </div> */}
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div>
+          <div className='mainImageContainer'>
+            {/* <div id='mainImage' > */}
+              <img src={this.state.currentImage} id='mainImage' onClick={this.zoomIn}/>
+            {/* </div> */}
+          </div>
+          {/* <div className = 'zoom'>
+          </div> */}
+        </div>
+      )
+    }
   }
 }
 
