c880059e00646eebe17e7bcc765793f466949a05
Shelly <pxshelly@gmail.com>
2019-10-21 03:32:54 -0700

    Rename classnames

--- client/src/GalleryImage.jsx
+++ client/src/GalleryImage.jsx
@@ -17,13 +17,13 @@ class GalleryImage extends React.Component {
     if (this.state.hover) {
       return (
         <li className="gallery-li-hover">
-          <img src={this.props.image} className="gallery-image-size" onMouseLeave={this.hoverImage} onClick={(event) => {this.props.changeImage(event.target.src)}} />
+          <img src={this.props.image} className="gallery-image" onMouseLeave={this.hoverImage} onClick={(event) => {this.props.changeImage(event.target.src)}} />
         </li>
       )
     } else {
       return (
         <li className="gallery-li">
-          <img src={this.props.image} className="gallery-image-size" onMouseEnter={this.hoverImage} />
+          <img src={this.props.image} className="gallery-image" onMouseEnter={this.hoverImage} />
         </li>
       )
     }
